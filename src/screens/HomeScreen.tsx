2d6e68f7ae6232e73a32b66075587bf23e181346
NeichS <nachoagusss1@gmail.com>
2025-05-16 20:49:36 -0300

    mejor formateo codigo

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,88 +1,169 @@
-import {  ScrollView, StatusBar, Text, TouchableOpacity, View } from "react-native";
+import {
+  ScrollView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import * as Font from "expo-font";
 import { CajaJuego } from "./components/CajaJuego";
 import { useState, useEffect } from "react";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { getGeneroPorId, IGeneroContenidoAudiovisual } from "@/src/data/generosContenidoAudiovisual";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import {
+  getGeneroPorId,
+  IGeneroContenidoAudiovisual,
+} from "@/src/data/generosContenidoAudiovisual";
 import { Generos } from "./components/Generos";
-import { getTipoPorId, ITipoContenidoAudiovisual } from "@/src/data/tiposContenidoAudiovisual";
-import { ContenidoAudiovisual, ContenidoAudiovisualMapped, contenidosAudiovisuales } from "@/src/data/contenidosAudiovisuales";
+import {
+  getTipoPorId,
+  ITipoContenidoAudiovisual,
+} from "@/src/data/tiposContenidoAudiovisual";
+import {
+  ContenidoAudiovisual,
+  ContenidoAudiovisualMapped,
+  contenidosAudiovisuales,
+} from "@/src/data/contenidosAudiovisuales";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-
-const series : ContenidoAudiovisualMapped[] = mapContenidoAudiovisual(contenidosAudiovisuales.filter((contenido) => contenido.tipoId === 1));
-const pelis : ContenidoAudiovisualMapped[] = mapContenidoAudiovisual(contenidosAudiovisuales.filter((contenido) => contenido.tipoId === 2));
-const animes : ContenidoAudiovisualMapped[] = mapContenidoAudiovisual(contenidosAudiovisuales.filter((contenido) => contenido.tipoId === 3));
+const series: ContenidoAudiovisualMapped[] = mapContenidoAudiovisual(
+  contenidosAudiovisuales.filter((contenido) => contenido.tipoId === 1)
+);
+const pelis: ContenidoAudiovisualMapped[] = mapContenidoAudiovisual(
+  contenidosAudiovisuales.filter((contenido) => contenido.tipoId === 2)
+);
+const animes: ContenidoAudiovisualMapped[] = mapContenidoAudiovisual(
+  contenidosAudiovisuales.filter((contenido) => contenido.tipoId === 3)
+);
 
 export function HomeScreen() {
-
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
-    Font.loadAsync({ PressStart: require("@/assets/fonts/PressStart2P-Regular.ttf") })
-      .then(() => setLoaded(true));
+    Font.loadAsync({
+      PressStart: require("@/assets/fonts/PressStart2P-Regular.ttf"),
+    }).then(() => setLoaded(true));
   }, []);
-  if (!loaded) return null; 
+  if (!loaded) return null;
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#1A1F2C", padding : 1, gap: 5 }}>
-            <StatusBar
-        backgroundColor="#1A1F2C"
+    <SafeAreaView
+      edges={["top"]}
+      style={{ flex: 1, backgroundColor: "#1A1F2C", gap: 5, paddingBottom: 20 }}
+    >
+      <StatusBar
         barStyle="light-content"
+        translucent={true}
+        backgroundColor="transparent"
       />
-      <View style={{ flexDirection: "row", justifyContent: "space-between", margin : 4 , padding:4}}>
-        <Text style={{ color: "#6E59A5", fontSize: 24, fontWeight: 500, fontFamily: "PressStart" }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          margin: 4,
+          padding: 4,
+        }}
+      >
+        <Text
+          style={{
+            color: "#6E59A5",
+            fontSize: 24,
+            fontWeight: 500,
+            fontFamily: "PressStart",
+          }}
+        >
           Pixdex
         </Text>
 
-        <View style={{ backgroundColor: "#6E59A5", padding : 5, borderWidth:1, borderLeftColor:"#9B87F5", borderTopColor:"#9B87F5", width: 160 }}>
+        <View
+          style={{
+            backgroundColor: "#6E59A5",
+            padding: 5,
+            borderWidth: 1,
+            borderLeftColor: "#9B87F5",
+            borderTopColor: "#9B87F5",
+            width: 160,
+          }}
+        >
           <TouchableOpacity>
-            <View style={{ flexDirection: "row", justifyContent: "center" ,alignItems: "center", gap: 8 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 8,
+              }}
+            >
               <View>
-                <Ionicons name="filter" size={20} color="black" style={{color:"white"}} />
+                <Ionicons
+                  name="filter"
+                  size={20}
+                  color="black"
+                  style={{ color: "white" }}
+                />
               </View>
-            <View>
-            <Text style={{color: "white", textAlign:"center", fontFamily :"PressStart", fontSize:12}}>FILTRAR</Text>
-            </View>
+              <View>
+                <Text
+                  style={{
+                    color: "white",
+                    textAlign: "center",
+                    fontFamily: "PressStart",
+                    fontSize: 12,
+                  }}
+                >
+                  FILTRAR
+                </Text>
+              </View>
             </View>
           </TouchableOpacity>
         </View>
       </View>
 
-      <View style={{ flexDirection : "row", gap: 4, margin : 2, padding: 4}}>
-        <CajaJuego title="Desafio del ahorcado" description="Adivina los títulos letra por letra ¿Cuántos puedes identificar?" bgColor="#6E59A5"/>
-        <CajaJuego title="Pixel reveal" description="Identifica títulos desde imagenes pixeladas. ¡Pon a prueba tu memoria visual!" bgColor="#5FD068"/>
+      <View style={{ flexDirection: "row", gap: 4, padding: 4 }}>
+        <CajaJuego
+          title="Desafio del ahorcado"
+          description="Adivina los títulos letra por letra ¿Cuántos puedes identificar?"
+          bgColor="#6E59A5"
+        />
+        <CajaJuego
+          title="Pixel reveal"
+          description="Identifica títulos desde imagenes pixeladas. ¡Pon a prueba tu memoria visual!"
+          bgColor="#5FD068"
+        />
       </View>
 
-      <View style={{height: 590, margin : 5}}>
-        <ScrollView contentContainerStyle={{gap:15}}>
-          <Generos lista={series} generoTitulo="Series"/>
-          <Generos lista={animes} generoTitulo="Animes"/> 
-          <Generos lista={pelis} generoTitulo="Peliculas"/> 
+      <View style={{ height: 590, margin: 5 }}>
+        <ScrollView contentContainerStyle={{ gap: 15, paddingBottom: 70 }}>
+          <Generos lista={series} generoTitulo="Series" />
+          <Generos lista={animes} generoTitulo="Animes" />
+          <Generos lista={pelis} generoTitulo="Peliculas" />
         </ScrollView>
       </View>
-    </View>
+
+      <View style={{}}></View>
+    </SafeAreaView>
   );
 }
 
-function mapContenidoAudiovisual(contenido : ContenidoAudiovisual[]) : ContenidoAudiovisualMapped[]{
-  
-  const contenidoAudiovisualMapeado : ContenidoAudiovisualMapped[] =
-  contenido.map((contenido) => {
-    const tipo : ITipoContenidoAudiovisual = getTipoPorId(contenido.tipoId);
-    let temp : IGeneroContenidoAudiovisual[] = [];
-    contenido.generos.forEach(generoId => {
-      temp.push(getGeneroPorId(generoId));
+function mapContenidoAudiovisual(
+  contenido: ContenidoAudiovisual[]
+): ContenidoAudiovisualMapped[] {
+  const contenidoAudiovisualMapeado: ContenidoAudiovisualMapped[] =
+    contenido.map((contenido) => {
+      const tipo: ITipoContenidoAudiovisual = getTipoPorId(contenido.tipoId);
+      let temp: IGeneroContenidoAudiovisual[] = [];
+      contenido.generos.forEach((generoId) => {
+        temp.push(getGeneroPorId(generoId));
+      });
+      const generos: IGeneroContenidoAudiovisual[] = temp;
+
+      return {
+        id: contenido.id,
+        nombre: contenido.nombre,
+        descripcion: contenido.descripcion,
+        generos: generos,
+        tipo: tipo,
+        imageUrl: contenido.imageUrl,
+      } as ContenidoAudiovisualMapped;
     });
-    const generos : IGeneroContenidoAudiovisual[] = temp;
 
-    return {
-      id: contenido.id,
-      nombre: contenido.nombre,
-      descripcion: contenido.descripcion,
-      generos: generos,
-      tipo: tipo,
-      imageUrl: contenido.imageUrl,
-    } as ContenidoAudiovisualMapped;
-  })
-
-  return contenidoAudiovisualMapeado; 
+  return contenidoAudiovisualMapeado;
 }
--- src/screens/components/Generos.tsx
+++ src/screens/components/Generos.tsx
@@ -2,47 +2,51 @@ import { FlatList, Text, View } from "react-native";
 import { TarjetaProducto } from "./TarjetaProducto";
 import { ContenidoAudiovisualMapped } from "@/src/data/contenidosAudiovisuales";
 
-export function Generos({lista, generoTitulo} :{lista : ContenidoAudiovisualMapped[], generoTitulo : string}) {
+export function Generos({
+  lista,
+  generoTitulo,
+}: {
+  lista: ContenidoAudiovisualMapped[];
+  generoTitulo: string;
+}) {
   return (
-    <View>
+    <View
+      style={{
+        borderWidth: 4,
+        borderColor: "#403E43",
+        marginTop: 10,
+        marginHorizontal: 5,
+      }}
+    >
       <View
         style={{
-          borderWidth: 4,
-          borderColor: "#403E43",
-          marginTop: 10,
-          marginHorizontal: 5,
+          backgroundColor: "#6E59A5",
+          marginTop: -15,
+          marginLeft: 10,
+          alignSelf: "flex-start",
+          borderWidth: 1,
+          borderColor: "#9B87F5",
         }}
       >
-        <View
+        <Text
           style={{
-            backgroundColor: "#6E59A5",
-            marginTop: -10,
-            marginLeft: 10,
-            alignSelf: "flex-start",
-            borderWidth: 1,
-            borderColor: "#9B87F5",
+            color: "white",
+            fontSize: 12,
+            margin: 4,
+            fontFamily: "PressStart",
           }}
         >
-          <Text
-            style={{
-              color: "white",
-              fontSize: 12,
-              margin: 4,
-              fontFamily: "PressStart",
-            }}
-          >
-            {generoTitulo}
-          </Text>
-        </View>
-        <FlatList
-          contentContainerStyle={{ gap: 8, margin: 10 }}
-          data={lista}
-          horizontal // habilita scroll horizontal
-          keyExtractor={(item) => item.id.toString()} // cada item debe tener key única
-          renderItem={({ item }) => <TarjetaProducto detail={item} />}
-          showsHorizontalScrollIndicator={false}
-        />
+          {generoTitulo}
+        </Text>
       </View>
+      <FlatList
+        contentContainerStyle={{ gap: 8, margin: 10 }}
+        data={lista}
+        horizontal // habilita scroll horizontal
+        keyExtractor={(item) => item.id.toString()} // cada item debe tener key única
+        renderItem={({ item }) => <TarjetaProducto detail={item} />}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   );
 }
