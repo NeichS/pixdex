c4796245e377e0962586cd5a1f8627af58b5bae2
Ignacio Sanchez <nachoagusss1@gmail.com>
2025-07-12 13:53:53 -0300

    mejora visual login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,99 +1,123 @@
-import React, { useState } from 'react'
-import { Alert, StyleSheet, View, AppState } from 'react-native'
-import { supabase } from '../lib/supabase'
-import { Input } from '@rneui/themed'
-import { Button } from './components/Button'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { useState } from "react";
+import { Alert, StyleSheet, View, AppState, TextInput } from "react-native";
+import { supabase } from "../lib/supabase";
+import { Button } from "./components/Button";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextPressStart2P } from "./components/TextPressStart2P";
 
 // Tells Supabase Auth to continuously refresh the session automatically if
 // the app is in the foreground. When this is added, you will continue to receive
 // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
 // if the user's session is terminated. This should only be registered once.
-AppState.addEventListener('change', (state) => {
-  if (state === 'active') {
-    supabase.auth.startAutoRefresh()
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
   } else {
-    supabase.auth.stopAutoRefresh()
+    supabase.auth.stopAutoRefresh();
   }
-})
+});
 
 export function LoginScreen() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function signInWithEmail() {
-    setLoading(true)
+    setLoading(true);
     const { error } = await supabase.auth.signInWithPassword({
       email: email,
       password: password,
-    })
+    });
 
-    if (error) Alert.alert(error.message)
-    setLoading(false)
+    if (error) Alert.alert(error.message);
+    setLoading(false);
   }
 
   async function signUpWithEmail() {
-    setLoading(true)
+    setLoading(true);
     const {
       data: { session },
       error,
     } = await supabase.auth.signUp({
       email: email,
       password: password,
-    })
+    });
 
-    if (error) Alert.alert(error.message)
-    if (!session) Alert.alert('Please check your inbox for email verification!')
-    setLoading(false)
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert("Please check your inbox for email verification!");
+    setLoading(false);
   }
 
   return (
-    <SafeAreaView edges={["top", "bottom"]} style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+    <SafeAreaView edges={["bottom", "top"]} style={styles.container}>
+      <TextPressStart2P style={styles.title}>
+        WELCOME TO PIXDEX
+      </TextPressStart2P>
+      <View style={styles.inputsContainer}>
+        <TextInput
           onChangeText={(text) => setEmail(text)}
           value={email}
+          style={styles.input}
           placeholder="email@address.com"
-          autoCapitalize={'none'}
+          placeholderTextColor="#8E9196"
+          autoCapitalize={"none"}
         />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+        <TextInput
           onChangeText={(text) => setPassword(text)}
           value={password}
+          style={styles.input}
           secureTextEntry={true}
           placeholder="Password"
-          autoCapitalize={'none'}
+          placeholderTextColor="#8E9196"
+          autoCapitalize={"none"}
         />
       </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button label="SIGN IN" disabled={loading} action={() => signInWithEmail()} />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button label="SIGN UP" disabled={loading} action={() => signUpWithEmail()} />
+      <View style={styles.buttons}>
+        <Button
+          label="SIGN UP"
+          disabled={loading}
+          action={() => signUpWithEmail()}
+          background="#5FD068"
+        />
+        <Button
+          label="SIGN IN"
+          disabled={loading}
+          action={() => signInWithEmail()}
+        />
       </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#1A1F2C",
-    marginTop: 40,
     padding: 12,
     flex: 1,
+    justifyContent: "center",
   },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: 'stretch',
+  title : {
+    color: "#6E59A5",
+    fontSize: 22,
+    textAlign: "center",
+    marginBottom: 20,
   },
-  mt20: {
-    marginTop: 20,
+  inputsContainer: {
+    flexDirection: "column",
+    gap: 10,
+    marginBottom: 20,
   },
-})
+  input: {
+    borderColor: "#6E59A5",
+    borderWidth: 1,
+    height: 40,
+    color: "#FFFFFF",
+    padding: 10,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    gap: 10,
+  },
+});
